20f23581587927ae45c0bd55c6d00841508653b6
SORAAAAAAAAA <rcbriones95@gmail.com>
2026-05-17 00:40:22 +0800

    feat(MainMenu): Initial commit with MainMenu

---
+++ scenes/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_new_button_pressed() -> void:
+	# This should start the new scene for the game
+	pass # Replace with function body.
+
+
+func _on_continue_button_pressed() -> void:
+	# This should continue the saved progress of the game
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	# opens the settings menu
+	pass # Replace with function body.
